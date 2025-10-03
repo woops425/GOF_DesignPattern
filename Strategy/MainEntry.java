382e1c21a4952f7c3ad03bfd356f566f0bb29b0a
woops425 <162817809+woops425@users.noreply.github.com>
2025-10-03 22:53:31 +0900

    Update MainEntry.java

--- Strategy/MainEntry.java
+++ Strategy/MainEntry.java
@@ -21,9 +21,11 @@ public class MainEntry {
 
 기능 확장에는 열려(Open)있고, 기존 코드의 변경에는 닫혀(Closed)있게 되어 유지보수가 매우 용이해집니다.
 
+
 알고리즘 교체의 용이성
 
-        MainEntry에서 보듯이, cal.print(new SimpleSumStrategy(), 10)를 cal.print(new GaussSumStrategy(), 10)로 바꾸는 것처럼, 클라이언트(사용자)는 필요에 따라 언제든지 알고리즘(전략)을 쉽게 교체하여 사용할 수 있습니다.
+MainEntry에서 보듯이, cal.print(new SimpleSumStrategy(), 10)를 cal.print(new GaussSumStrategy(), 10)로 바꾸는 것처럼, 클라이언트(사용자)는 필요에 따라 언제든지 알고리즘(전략)을 쉽게 교체하여 사용할 수 있습니다.
+
 
 코드의 응집도 향상 및 분리
 
@@ -31,11 +33,13 @@ public class MainEntry {
 
 이를 통해 각 알고리즘은 자신만의 책임(합계 계산)에만 집중하게 되어 **단일 책임 원칙(SRP, Single Responsibility Principle)**을 잘 따르게 됩니다. 코드를 이해하고 관리하기가 훨씬 수월해집니다.
 
-상속 대신 위임(Delegation) 활용
+
+상속 대신 *위임(Delegation)* 활용
 
 SumPrinter가 합계를 구하는 방법을 상속받는 대신, SumStrategy 객체에게 계산을 '위임'합니다.
 
 이를 통해 상속으로 인한 강한 결합을 피하고, 더 유연하고 확장성 있는 구조를 만들 수 있습니다.
+
 
 테스트 용이성
 
